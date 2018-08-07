8b5579e87cdbb698657bf77cca81ed4ba856d20b
Varnit Goel <17356399+varnitgoel@users.noreply.github.com>
2018-08-07 17:25:55 +0100

    Add files via upload
    Initializing main function

--- main.py
+++ main.py
@@ -14,47 +14,58 @@ appl = Flask(__name__)
 api = Api(appl)
 
 class main(): ##Main Class
-    def __init__(self):
-        self.user_no = input("Enter repository number: ")
-        self.git_name = []
-        self.get_repo = []
+    def __init__():
+        user_repo_no = input("Enter number of repositories/users: ")
+        git_name = []
+        git_repo = []
+        git_user = {}
         
-        while(self.no_of_users != 0):
+        while(user_repo_no != 0):
+            
             
             
 #**********************************************************************************************
 #1. Total number of commit contributions to any project to which a user has a contributed.
 #**********************************************************************************************
-            
+@app.route("/")   
          
    
 #**********************************************************************************************
 #2. Total number of commit contributions as above, but restricted to projects that are 
 #   members of the original submitted set.             
 #**********************************************************************************************
-
+@app.route("/")
 
 
 #**********************************************************************************************
 #3. The number of known programming languages for each user (presuming that the languages of 
 #  any repository committed to are known to the user)
 #**********************************************************************************************  
-
+@app.route("/")
 
 
 #**********************************************************************************************
 #4. The weekly commit rate of users (provide a weekly rank ordering) for the submitted 
 #  project set, for 2018.  
 #**********************************************************************************************
-
+@app.route("/")
 
 
 #**********************************************************************************************
 #5. The average commit rate of each user to any project, for 2018."""
 #**********************************************************************************************
-
+@app.route("/")
 
 #**********************************************************************************************
 #6. The total number of collaborators in 2018 (ie. a count of other users who have 
 #   contributed to any project that the user has contributed to).
 #**********************************************************************************************
+@app.route("/")
+
+
+#**********************************************************************************************
+
+def master(): 
+    
+if __name__ == "__main__":
+    appl.run()
