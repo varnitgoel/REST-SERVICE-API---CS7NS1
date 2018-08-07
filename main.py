c49cdeffac8724ee719ab4d98bf04701d11a1979
Varnit Goel <17356399+varnitgoel@users.noreply.github.com>
2018-08-07 14:38:01 +0100

    Main File
    Starting with the logic part of the 6 questions

--- main.py
+++ main.py
@@ -17,4 +17,44 @@ class main(): ##Main Class
     def __init__(self):
         self.user_no = input("Enter repository number: ")
         self.git_name = []
+        self.get_repo = []
         
+        while(self.no_of_users != 0):
+            
+            
+#**********************************************************************************************
+#1. Total number of commit contributions to any project to which a user has a contributed.
+#**********************************************************************************************
+            
+         
+   
+#**********************************************************************************************
+#2. Total number of commit contributions as above, but restricted to projects that are 
+#   members of the original submitted set.             
+#**********************************************************************************************
+
+
+
+#**********************************************************************************************
+#3. The number of known programming languages for each user (presuming that the languages of 
+#  any repository committed to are known to the user)
+#**********************************************************************************************  
+
+
+
+#**********************************************************************************************
+#4. The weekly commit rate of users (provide a weekly rank ordering) for the submitted 
+#  project set, for 2018.  
+#**********************************************************************************************
+
+
+
+#**********************************************************************************************
+#5. The average commit rate of each user to any project, for 2018."""
+#**********************************************************************************************
+
+
+#**********************************************************************************************
+#6. The total number of collaborators in 2018 (ie. a count of other users who have 
+#   contributed to any project that the user has contributed to).
+#**********************************************************************************************
