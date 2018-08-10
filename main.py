31cdf3dad664996305fe8bc13f939fdb29e37f57
Varnit Goel <17356399+varnitgoel@users.noreply.github.com>
2018-08-10 16:06:23 +0100

    solution to criteria 1 //initial phase

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ import time, getpass
 appl = Flask(__name__)
 api = Api(appl)
 
-class main(): ##Main Class
+class main(): #Main Class
     def __init__():
-        user_repo_no = input("Enter number of repositories/users: ")
+        user_repo_no = input("Enter number of repositories/users: ") #user input for no of repo
         git_name = []
         git_repo = []
         git_user = {}
@@ -24,56 +24,56 @@ class main(): ##Main Class
             
             
             
-#**********************************************************************************************
-#1. Total number of commit contributions to any project to which a user has a contributed.
-#**********************************************************************************************
+##1. Total number of commit contributions to any project to which a user has a contributed.
 @app.route("/criteria1")
 
-def criteria1():
+def count_user_commits(user):
+    r = requests.get('https://api.github.com/users/%s/repos' % user)
+    repos = json.loads(r.content)
+
+    
          
    
-#**********************************************************************************************
-#2. Total number of commit contributions as above, but restricted to projects that are 
-#   members of the original submitted set.             
-#**********************************************************************************************
+##2. Total number of commit contributions as above, but restricted to projects that are 
+##   members of the original submitted set.             
 @app.route("/criteria2")
 
 def criteria2():
 
+    
 
-#**********************************************************************************************
-#3. The number of known programming languages for each user (presuming that the languages of 
-#  any repository committed to are known to the user)
-#**********************************************************************************************  
+##3. The number of known programming languages for each user (presuming that the languages of 
+##  any repository committed to are known to the user)
 @app.route("/criteria3")
+
+GET https://api.github.com/repos/dotnet/corefx/languages
 
 def criteria3():
 
-#**********************************************************************************************
-#4. The weekly commit rate of users (provide a weekly rank ordering) for the submitted 
-#  project set, for 2018.  
-#**********************************************************************************************
+
+##4. The weekly commit rate of users (provide a weekly rank ordering) for the submitted 
+##  project set, for 2018.  
 @app.route("/criteria4")
 
 def criteria4():
 
-#**********************************************************************************************
-#5. The average commit rate of each user to any project, for 2018."""
-#**********************************************************************************************
+
+##5. The average commit rate of each user to any project, for 2018."""
 @app.route("/criteria5")
 
 def criteria5():
 
-#**********************************************************************************************
-#6. The total number of collaborators in 2018 (ie. a count of other users who have 
-#   contributed to any project that the user has contributed to).
-#**********************************************************************************************
+    
+##6. The total number of collaborators in 2018 (ie. a count of other users who have 
+##  contributed to any project that the user has contributed to).
 @app.route("/criteria6")
 
 def criteria6():
 
-#**********************************************************************************************
-
+    
+##**********************************************************************************************
+##Main function
+    
 def master(): 
     
 if __name__ == "__main__":
