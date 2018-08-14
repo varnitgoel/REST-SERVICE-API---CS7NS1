8ab103779189e0eb524da4edf0e6250eae3fa5de
Varnit Goel <17356399+varnitgoel@users.noreply.github.com>
2018-08-14 22:57:12 +0530

    Little updates and variable fixes

--- main.py
+++ main.py
@@ -8,35 +8,35 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import json, requests, datetime, getpass, subprocess
 
-##*************************************For Email module******************************************** 
+##********************************For Email module*************************************** 
 import smtplib
 from smtplib import SMTPException
 from email.message import EmailMessage
-##*************************************************************************************************
+##***************************************************************************************
 
 appl = Flask(__name__)
 api = Api(appl)
 
 class main(): #Main Class
     def __init__():
-        user_repo_no = int(input("Enter number of repositories/users: ")) #user input for no of repo
+        user_user_no = int(input("Enter number of repositories/users: ")) #user input for no of repo
         git_user_name = []
         repo_name = []
         git_user = []
-        while(user_repo_no != 0):
+        while(user_user_no != 0):
             name_of_user = input('Enter username: ')
             git_user_name.append(name_of_user)
             repository_name = input('Enter repository name: ')
             repo_name.append(repository_name)
-            user_repo_no = user_repo_no - 1
-        git_user.append(dict(zip(git_user_name,repo_name))) ##Created a dictionery to store
+            user_user_no = user_user_no - 1
+            git_user.append(dict(zip(git_user_name,repo_name))) ##Created a dictionery to store
             
             
-##********************************************************************************************************            
+##***************************************************************************************          
 ##1. Total number of commit contributions to any project to which a user has a contributed.
-@app.route("/criteria1", methods=['GET'])
+@appl.route("/criteria1", methods=['GET'])
 def criteria1():
-    git_count = {}
+    commit_count = {}
 
 #Function to get all the repos of a user
 def count_user_commits(user):
@@ -71,56 +71,58 @@ def find_next(link):
         a, b = l.split(';')
         if b.strip() == 'rel="next"':
             return a.strip()[1:-1]
-##********************************************************************************************************			
-			
-##********************************************************************************************************   
+
+   
+##***************************************************************************************    
 ##2. Total number of commit contributions as above, but restricted to projects that are 
 ##   members of the original submitted set.             
-@app.route("/criteria2", methods=['GET'])
+@appl.route("/criteria2", methods=['GET'])
+
 def criteria2():
-    git_count = {}
+    commit_count = {}
     
 
+
+##***************************************************************************************
 ##3. The number of known programming languages for each user (presuming that the languages of 
 ##   any repository committed to are known to the user)
-@app.route("/criteria3", methods=['GET'])
+@appl.route("/criteria3", methods=['GET'])
 def criteria3():
-    prog_lang = {} ##List of languages used
-	## Using this api 
-	## GET https://api.github.com/repos/
-	
+    prog_lang = {}
     
 
-    
+
+##***************************************************************************************    
 ##4. The weekly commit rate of users (provide a weekly rank ordering) for the submitted 
 ##  project set, for 2018.  
-@app.route("/criteria4", methods=['GET'])
+@appl.route("/criteria4", methods=['GET'])
 
 def criteria4():
-    commit_rate = {} ##The commit rate list
-	##Using thi API
-	##https://api.github.com/users/
+    commit_rate = {}
 
 
+
+##***************************************************************************************
 ##5. The average commit rate of each user to any project, for 2018."""
-@app.route("/criteria5", methods=['GET'])
+@appl.route("/criteria5", methods=['GET'])
 
 def criteria5():
-    average_commit = {} ##List of average commits
+    average_commit = {}
 
 
-    
+
+##***************************************************************************************   
 ##6. The total number of collaborators in 2018 (ie. a count of other users who have 
 ##  contributed to any project that the user has contributed to).
-@app.route("/criteria6", methods=['GET'])
+@appl.route("/criteria6", methods=['GET'])
 
 def criteria6():
-    collab = {} ##List of Collaborators
-
-	
-	
-
-##7. Email
+    collab = {}
+    
+    
+    
+##***************************************************************************************    
+##7. Email Module
 @app.route('/mail', methods = ['POST','GET'])
 def criteria7():
 	msg['Subject'] = 'The contents of file'
@@ -132,10 +134,10 @@ s = smtplib.SMTP('localhost')
 s.send_message(msg)
 s.quit()
 
-
+    
 
 ##**********************************************************************************************
-##Main function
+## Main function
     
 def master(): 
     
